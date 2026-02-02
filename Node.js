04ca02120d1cccfc2012c094562d384fa52e2fb1
lemonadegxhi <husemanchris6@gmail.com>
2026-02-02 16:14:45 -0600

    Attempt at connecting to Mongo

--- Node.js
+++ Node.js
@@ -1,1 +1,15 @@
-//NodeJS
+const mongoose = require('mongoose');
+
+const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/myDatabase';
+
+async function connectDB() {
+    try {
+        await mongoose.connect(uri);
+        console.log('MongoDB connected with Mongoose');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
